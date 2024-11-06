2096ce5b7b0ae6937928a60b2eb304f41386ccbc
AlessioMura <alessiomura03@gmail.com>
2024-11-06 14:38:13 +0100

    fixes

--- Bonus/app.js
+++ Bonus/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const instrumentsRouter = require('./router/instruments')
+app.use(express.json());
 
 app.listen(3002, () => {
     console.log("Server started on port 3002");
 });
+
+app.use('/instruments', instrumentsRouter)
--- Bonus/router/instruments.js
+++ Bonus/router/instruments.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const instrumentsController = require('../controllers/instruments.js')
+const instrumentsController = require('../controller/instrumentsController.js')
 
 router.get('/', instrumentsController.index);
+
+module.exports = router;

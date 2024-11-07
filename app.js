36e075749a0a4e79b32d6519d08a11f2e19aee07
AlessioMura <alessiomura03@gmail.com>
2024-11-07 14:29:15 +0100

    added logger.js

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const express = require('express')
-const app = express()
-const postsRouter = require('./router/posts.js')
-app.use(express.json())
+const express = require('express');
+const app = express();
+const postsRouter = require('./router/posts.js');
+const notFoundMiddleware = require('./middlewares/notFound.js');
+const loggerMiddleware =require('./middlewares/loggerMiddleware.js');
+app.use(express.json());
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.listen(3000, () => {
     console.log(`server started on port 3000`);
 
 })
 
+app.use('/posts', loggerMiddleware);
+
 app.use('/posts', postsRouter);
+
+app.use(notFoundMiddleware);
